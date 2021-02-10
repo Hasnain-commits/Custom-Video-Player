eb2fd46f6fb28f8124472f9b136c3a902a9425af
Hasnain-Commits <hasnainhayat2002@gmail.com>
2021-02-09 19:42:26 -0500

    Made custom video player

---
+++ app.js
@@ -0,0 +1,69 @@
+const video = document.querySelector("#video");
+const play = document.querySelector("#play");
+const stop = document.querySelector("#stop");
+const timeStamp = document.querySelector("#timestamp");
+const progress = document.querySelector("#progress");
+const audio = document.querySelector("#audio");
+const auds = document.querySelector("sup");
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+video.volume = audio.value;
+auds.innerHTML = `${audio.value * 100}%`;
+
+audio.addEventListener("change", () => {
+    video.volume = audio.value;
+    auds.innerHTML = `${audio.value * 100}%`;
+})
+
+
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress)
+
+
+function toggleVideoStatus () {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updatePlayIcon () {
+    if (video.paused) {
+        play.innerHTML = "<i class='fa fa-play fa-2x'></i>"
+    } else {
+        play.innerHTML = "<i class='fa fa-pause  fa-2x'></i>"
+    }
+}
+
+function updateProgress () {
+    progress.value = (video.currentTime / video.duration) * 100;
+    let min = Math.floor(video.currentTime / 60) ;
+
+    if (min < 10) {
+        min = "0" + String(min);
+    }
+
+    let sec = Math.floor(video.currentTime % 60) ;
+
+    if (sec < 10) {
+        sec = "0" + String(sec);
+    }
+
+    timeStamp.innerHTML = `${min}:${sec}`;
+}
+
+function setVideoProgress () {
+    video.currentTime = (+progress.value) * video.duration / 100;
+}
+
+function stopVideo () {
+    video.currentTime = 0;
+    video.pause();
+}
